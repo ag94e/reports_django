6800c81e7b6ff45a85bb81e948254490043969fd
ag94e <a.guerrero.esp94@gmail.com>
2021-03-14 21:16:20 -0600

    modified gitignore, and create other model

--- diario/admin.py
+++ diario/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from diario.models import Perfi, Diario
+from diario.models import Perfil, Diario, DiarioPorFecha
 
 admin.site.register(Perfil)
 admin.site.register(Diario)
+admin.site.register(DiarioPorFecha)
--- diario/models.py
+++ diario/models.py
@@ -11,19 +11,38 @@ class Perfil(models.Model):
     ciudad = models.CharField(max_length=40)
     es_gerente = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name_plural = 'Perfiles'
+
     def __str__(self):
         return self.nombre
 
 class Diario(models.Model):
 
+    respuestas = [
+        ('SI', 'SI'),
+        ('NO', 'NO')
+    ]
     brm = models.ForeignKey(Perfil, on_delete=models.CASCADE)
     temperatura = models.CharField(max_length=5)
-    tos = models.BooleanField(default=False)
-    cabeza = models.BooleanField(default=False)
-    garganta = models.BooleanField(default=False)
-    restriado = models.BooleanField(default=False)
-    malestar_general = models.BooleanField(default=False)
-    respirar = models.BooleanField(default=False)
+    tos = models.CharField(max_length=2, choices=respuestas, default=None)
+    cabeza = models.CharField(max_length=2, choices=respuestas, default=None)
+    garganta = models.CharField(max_length=2, choices=respuestas, default=None)
+    restriado = models.CharField(max_length=2, choices=respuestas, default=None)
+    malestar_general = models.CharField(max_length=2, choices=respuestas, default=None)
+    respirar = models.CharField(max_length=2, choices=respuestas, default=None)
 
     def __str__(self):
         return self.brm.nombre
+
+class DiarioPorFecha(models.Model):
+
+    fecha = models.DateField(auto_now_add=True)
+    diario_persona = models.ManyToManyField(Diario)
+    gerente_turno = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Diario por fechas'
+
+    def __str__(self):
+        return f'{self.gerente_turno.nombre} : {self.fecha}'
